beeb8c5be1811dc8de9618cc7a4f3210bfae2f9f
Emile Cloostermans-Huwaert <cloostermansemile@gmail.com>
2025-05-09 11:34:38 +0200

    started working on userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,16 @@
 const pool = require('../utils/db');
 const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+const authenticateToken = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1] // extract token from header
+
+    if(!token) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+    jwt.verify(token, config.jwt_secret, (err, decodedUser) => {
+        if (err) return res.status(403).json({ error: "Invalid token" });
+    })
+    
+}
